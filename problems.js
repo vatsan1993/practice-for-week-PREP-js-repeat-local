9d99c98504e0e5ed3554e3fcb4eb154da8020072
Srivatsan <vatsan1993@gmail.com>
2023-02-18 05:51:33 +0530

    finished

--- problems.js
+++ problems.js
@@ -120,8 +120,15 @@ function aCounter(word) {
     return count;
     */
     // Your code here
-    let index = 0;
+
     let count = 0;
+    for(let index=0; index< word.length; index++ ){
+        let char = word[index];
+        if (char === "a" || char === "A") {
+        count += 1;
+        }
+    }
+    return count;
 };
 
 module.exports = {
